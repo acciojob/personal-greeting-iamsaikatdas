3b8afcc85e1e5cec72701b88e1e84217d224ce0c
Saikat Das <102199984+iamsaikatdas@users.noreply.github.com>
2023-07-11 08:40:50 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,14 @@
 
 import React from "react";
 import './../styles/App.css';
+import {useState} from 'react';
 
 const App = () => {
+  const [inputTxt, setInputTxt]=useState("");
   return (
     <div>
         {/* Do not remove the main div */}
+        <input onChange={(e)=> setInputTxt("Hello "+e.target.value)} />
     </div>
   )
 }
